49158f739fb96708d51bf7f4c3380af0d51489b8
April Ablon <ablonapril@gmail.com>
2017-05-27 14:42:31 -0700

    Remove util seed functions

--- server/test/instrumentModel.test.js
+++ server/test/instrumentModel.test.js
@@ -1,15 +1,4 @@
 const Instrument = require('../../db/models/instruments.js');
-const dbUtils = require('../../db/lib/utils.js');
-
-// Deletes all tables, creates new tables, and seeds tables with test data
-// beforeEach((done) => {
-//   dbUtils.rollbackMigrate(done);
-// });
-
-// // Resets database back to original settings
-// afterEach((done) => {
-//   dbUtils.rollback(done);
-// });
 
 test('Should be able to retrieve test data', (done) => {
   Instrument.forge().fetchAll()
--- server/test/profileModel.test.js
+++ server/test/profileModel.test.js
@@ -1,15 +1,4 @@
 const Profile = require('../../db/models/profiles.js');
-const dbUtils = require('../../db/lib/utils.js');
-
-// Deletes all tables, creates new tables, and seeds tables with test data
-// beforeEach((done) => {
-//   dbUtils.rollbackMigrate(done);
-// });
-
-// // Resets database back to original settings
-// afterEach((done) => {
-//   dbUtils.rollback(done);
-// });
 
 test('Should be able to retrieve test data', (done) => {
   Profile.forge().fetchAll()
