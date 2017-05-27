98b972e83a97082497ef28d0f7cf6d3ff148dd48
April Ablon <ablonapril@gmail.com>
2017-05-27 14:42:31 -0700

    Remove util seed functions

---
+++ server/test/instrumentModel.test.js
@@ -0,0 +1,38 @@
+const Instrument = require('../../db/models/instruments.js');
+
+test('Should be able to retrieve test data', (done) => {
+  Instrument.forge().fetchAll()
+      .then((results) => {
+        expect(results.length).toBe(1);
+        expect(results.at(0).get('id')).toBe(1);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+});
+
+test('Should verify that all instrument names are unique', (done) => {
+  Instrument.forge({
+    instrument_name: 'triangle',
+  }).save()
+  .then((result) => {
+    done(new Error('was not supposed to succeed: ', result));
+  })
+  .catch((err) => {
+    expect(err).toBeDefined();
+    done();
+  });
+});
+
+test('Should be able to delete a record', (done) => {
+  Instrument.where({ id: 1 }).destroy()
+    .then(() => Instrument.where({ id: 1 }).fetch())
+    .then((result) => {
+      expect(result).toBe(null);
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
