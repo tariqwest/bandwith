7dc01e9853a59db31db0e46e5d4e661f940d8220
calebcordry <caleb.cordry@gmail.com>
2017-05-26 20:05:37 -0700

    fix unfinished declaration

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,4 +3,4 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 
-export const requestLogin
+export const requestLogin = () => {};
