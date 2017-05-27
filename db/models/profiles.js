534f7b5e1eafb10187eb71547546b55fead3aaf2
Max Braz <braz.max@gmail.com>
2017-05-27 11:27:57 -0700

    add user fields into profile

--- db/models/profiles.js
+++ db/models/profiles.js
@@ -2,9 +2,17 @@ const db = require('../');
 
 const Profile = db.Model.extend({
   tableName: 'profiles',
-  auths() {
-    return this.hasMany('Auth');
-  },
+  auths: () => this.hasMany('Auth'),
+  song: () => this.hasOne('Song'),
+  video: () => this.hasOne('Video'),
+  photo: () => this.hasOne('Photo'),
+  connections: () => this.hasMany('Connection'),
+  chats: () => this.hasMany('Chat'),
+  instruments: () => this.belongsToMany('Instrument'),
+  genres: () => this.belongsToMany('Genre'),
+  influences: () => this.belongsToMany('Influence'),
+  preferred_instruments: () => this.belongsToMany('Instrument'),
+  preferred_genres: () => this.belongsToMany('Genre'),
 });
 
 module.exports = db.model('Profile', Profile);
