0c4850169ab5cb9fc2e680e10fc24247ea482ce5
Max Braz <braz.max@gmail.com>
2017-05-26 20:02:09 -0700

    change to seedInstrument

--- server/test/seedDB.test.js
+++ server/test/seedDB.test.js
@@ -1,5 +1,5 @@
 const model = require('../../db/seeds/instruments_seed.js');
 
 test('adds instruments to instruments table', () => {
-  expect(model.seed()).toBe(undefined);
+  expect(model.seedInstruments()).toBe(undefined);
 });
