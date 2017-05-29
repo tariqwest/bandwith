2a62e8433b5258ffe09f9f8a7998a938f6a704f4
April Ablon <ablonapril@gmail.com>
2017-05-29 12:26:11 -0700

    Change naming for relation properties

--- db/models/instruments.js
+++ db/models/instruments.js
@@ -2,8 +2,8 @@ const db = require('../');
 
 const Instrument = db.Model.extend({
   tableName: 'instruments',
-  preference_profile: () => this.belongsToMany('Profile'),
-  choice_profile: () => this.belongsToMany('Profile'),
+  preferenced_by: () => this.belongsToMany('Profile'),
+  player: () => this.belongsToMany('Profile'),
 });
 
 module.exports = db.model('Instrument', Instrument);
