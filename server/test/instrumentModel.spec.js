12559e5fc0be644279466357f8529a5b979188fb
April Ablon <ablonapril@gmail.com>
2017-05-29 12:51:23 -0700

    Capitalize assertion descriptions

--- server/test/instrumentModel.spec.js
+++ server/test/instrumentModel.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const Instrument = require('../../db/models/instruments.js');
 
 describe('Instrument Model', () => {
-  it('should be able to retrieve test data', (done) => {
+  it('Should be able to retrieve test data', (done) => {
     Instrument.forge({ instrument_name: 'triangle' }).save()
       .then(() => Instrument.where({ id: 1 }).fetch())
       .then((results) => {
@@ -15,7 +15,7 @@ describe('Instrument Model', () => {
       });
   });
 
-  it('should verify that all instrument names are unique', (done) => {
+  it('Should verify that all instrument names are unique', (done) => {
     Instrument.forge({ instrument_name: 'triangle' }).save()
       .then((result) => {
         done(new Error('was not supposed to succeed: ', result));
