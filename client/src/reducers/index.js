5b69467ed55c3fe39d33350bd36a0a3e90549499
calebcordry <caleb.cordry@gmail.com>
2017-05-30 20:07:00 -0700

    weird

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
-import redirectUrl from './redirectUrl';
 
 // We combine the reducers here
 const rootReducer = combineReducers({
   auth,
-  redirectUrl,
 });
 
 export default rootReducer;
