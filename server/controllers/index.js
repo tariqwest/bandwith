c45cf8455372a07e08547cc9ed7bc6ca6c342cfa
Max Braz <braz.max@gmail.com>
2017-05-31 15:45:31 -0700

    use signup instead of test

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
 module.exports.Profiles = require('./profiles');
 module.exports.Chats = require('./chats');
-module.exports.Test = require('./test');
 module.exports.Connections = require('./connections');
 module.exports.Preference = require('./preference');
+module.exports.Signup = require('./signup');
