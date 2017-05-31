14275b6386c718058548a820db0553a465c68cfd
April Ablon <ablonapril@gmail.com>
2017-05-30 19:46:56 -0700

    Add routes for Matches and Results components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Test from './Test';
+import Matches from './Matches';
+import Results from './Results';
+import Login from './Login';
+import Private from './Private';
+import PrivateRoute from './PrivateRoute';
 
 import {
   BrowserRouter as Router,
   Route,
   Link,
 } from 'react-router-dom';
-
-import Login from './Login';
-import Private from './Private';
-import PrivateRoute from './PrivateRoute';
 
 import store from '../store';
 
@@ -23,6 +25,9 @@ const App = () => (
         </ul>
         <Route path="/login" component={Login} />
         <PrivateRoute path="/private" component={Private} />
+        <Route path="/test" component={Test} />
+        <Route path="/connections" component={Matches} />
+        <Route path="/results" component={Results} />
       </div>
     </Router>
   </Provider>
