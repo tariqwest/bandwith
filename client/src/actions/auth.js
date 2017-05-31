928593721c61e40ccc6b89e1e97a09ab882167fe
calebcordry <caleb.cordry@gmail.com>
2017-05-30 20:01:19 -0700

    add user wip

---
+++ client/src/actions/auth.js
@@ -0,0 +1,38 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+
+export const requestLogin = () => ({
+  type: LOGIN_REQUEST,
+  isFetching: true,
+  isAuthenticated: false,
+});
+
+export const receiveLogin = user => ({
+  type: LOGIN_SUCCESS,
+  isFetching: false,
+  isAuthenticated: user.loggedIn,
+  userId: user.userId || null,
+});
+
+export const loginError = message => ({
+  type: LOGIN_FAILURE,
+  isFetching: false,
+  isAuthenticated: false,
+  message,
+});
+
+export const checkLogin = () => (dispatch) => {
+  dispatch(requestLogin());
+  return fetch('/auth/status')
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    })
+    .then(res => res.json())
+    .then(json => dispatch(receiveLogin(json)))
+    .catch(err => dispatch(loginError(err.message)));
+};
---
+++ client/src/actions/user.js
@@ -0,0 +1,34 @@
+export const USER_INFO_REQUEST = 'USER_INFO_REQUEST';
+export const USER_INFO_SUCCESS = 'USER_INFO_SUCCESS';
+export const USER_INFO_FAILURE = 'USER_INFO_FAILURE';
+
+export const requestUserInfo = () => ({
+  type: USER_INFO_REQUEST,
+  isFetching: true,
+});
+
+export const receiveUserInfo = userInfo => ({
+  type: USER_INFO_SUCCESS,
+  isFetching: false,
+  userInfo,
+});
+
+export const userInfoError = message => ({
+  type: USER_INFO_FAILURE,
+  isFetching: false,
+  message,
+});
+
+export const getUserInfo = userId => (dispatch) => {
+  dispatch(requestUserInfo());
+  return fetch(`/api/users${userId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    })
+    .then(res => res.json())
+    .then(json => dispatch(receiveUserInfo(json)))
+    .catch(err => dispatch(userInfoError(err.message)));
+};
