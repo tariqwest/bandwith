3a5d060e5ce4c422c01f6a4ab6d3e52f87e60f85
Max Braz <braz.max@gmail.com>
2017-05-31 14:42:32 -0700

    rename to signup

--- server/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const TestController = require('../controllers').Test;
-
-const router = express.Router();
-
-router.route('/')
-  .post(TestController.create);
-
-module.exports = router;
