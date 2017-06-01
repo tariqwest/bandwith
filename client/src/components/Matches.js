e1ed9d9ed62b4628020f55f6c940b835912e600e
April Ablon <ablonapril@gmail.com>
2017-06-01 16:31:14 -0700

    Updates Matches component to update matches state property with results from the database

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import MatchList from './MatchList';
-import dummyData from '../data/dummyData';
 
 class Matches extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      matches: dummyData,
+      matches: [],
     };
   }
 
-  componentDidMount() {
-    this.getMatches(this.setMatches.bind(this));
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps: ', nextProps);
+    if (nextProps.userId) {
+      console.log('executing get matches...');
+      this.getMatches(this.setMatches.bind(this), nextProps.userId);
+    }
   }
 
   setMatches(matches) {
+    console.log('setting matches: ', matches);
     this.setState({ matches });
   }
 
-  getMatches(callback) {
-    fetch(`/api/connections?userId=${this.props.userId}`)
-      .then((response) => {
-        console.log('Successful GET request to /connections: ', response);
-        // callback(response);
+  getMatches(callback, userId) {
+    axios.get(`/api/connections?userId=${userId}`)
+      .then((res) => {
+        console.log('Successful GET request to /api/connections: ', res.data);
+        callback(res.data);
       })
-      .catch(err => console.log('Bad GET request to /connections: ', err));
+      .catch(err => console.log('Bad GET request to /api/connections: ', err));
   }
 
   render() {
