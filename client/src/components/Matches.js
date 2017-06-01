98c984e8fdd1c798d165c0e081e5521fdeec5e82
April Ablon <ablonapril@gmail.com>
2017-05-31 19:21:01 -0700

    Add API call to server for initializing results

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -6,8 +6,26 @@ class Matches extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      userId: 2,
       matches: dummyData,
     };
+  }
+
+  componentDidMount() {
+    this.getMatches(this.setMatches.bind(this));
+  }
+
+  setMatches(matches) {
+    this.setState({ matches });
+  }
+
+  getMatches(callback) {
+    fetch(`/api/connections?userId=${this.state.userId}`)
+      .then((response) => {
+        console.log('Successful GET request to /connections: ', response);
+        // callback(response);
+      })
+      .catch(err => console.log('Bad GET request to /connections: ', err));
   }
 
   render() {
