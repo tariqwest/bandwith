6d79e53fac12de3361fd86a2c43fb38e1a21cb07
April Ablon <ablonapril@gmail.com>
2017-06-01 16:34:36 -0700

    Complete database query to return connections to client for logged in user

--- server/controllers/connections.js
+++ server/controllers/connections.js
@@ -1,33 +1,34 @@
 const models = require('../../db/models');
 
 module.exports.getAll = (req, res, done) => {
-  console.log('getting ALL connections... ', req.query);
-  // create store for ids or user data
   const store = [];
 
-  // query connections table for profile_id_1
-  models.Connection.where({ profile_id_1: req.query.userId }).fetchAll({ withRelated: ['profile_id_2'] })
+  models.Profile.where({ id: req.query.userId }).fetch({ withRelated: ['connections_1'] })
     .then((connections) => {
-      console.log('profile 1 connections: ', connections);
-      // push profile_id_2 value to store
-      // store.concat(connections);
-      // OR get related user info and push to store
+      connections.related('connections_1').models.map((connection) => {
+        return connection;
+      }).filter(connection => (
+        connection.pivot.attributes.likes_1_2 && connection.pivot.attributes.likes_2_1
+      )).forEach((match) => {
+        store.push(match.attributes);
+      });
     })
     .then(() => (
-      // query connections table for profile_id_2
-      models.Connection.where({ profile_id_2: req.query.userId }).fetchAll({ withRelated: ['profile_id_1'] })
+      models.Profile.where({ id: req.query.userId }).fetch({ withRelated: ['connections_2'] })
     ))
     .then((connections) => {
-      console.log('profile 2 connections: ', connections);
-      // push profile_id_1 value to store
-      // store.concat(connections);
-      // OR get related user info and push to store
+      connections.related('connections_2').models.map((connection) => {
+        return connection;
+      }).filter(connection => (
+        connection.pivot.attributes.likes_1_2 && connection.pivot.attributes.likes_2_1
+      )).forEach((match) => {
+        store.push(match.attributes);
+      });
+    })
+    .then(() => {
+      res.send(store);
     })
     .catch((err) => {
       done(err);
     });
-
-  // scenario 1 where we are only able to get user ids
-  // query profiles table with all user ids
-  res.sendStatus(200);
 };
