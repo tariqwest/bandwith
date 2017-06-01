42253aa6a7ee0b3bc84a0c583caf78ea07f042a9
April Ablon <ablonapril@gmail.com>
2017-06-01 16:33:24 -0700

    Update Bookshelf connections model for many-to-many relationship for users connections

--- db/models/connections.js
+++ db/models/connections.js
@@ -3,10 +3,10 @@ const db = require('../');
 const Connection = db.Model.extend({
   tableName: 'connections',
   profile_id_1: function() {
-    return this.belongsTo('Profile');
+    return this.belongsTo('Profile', 'profile_id', 'profile_id');
   },
   profile_id_2: function() {
-    return this.belongsTo('Profile');
+    return this.belongsTo('Profile', 'profile_id', 'profile_id');
   },
 });
 
