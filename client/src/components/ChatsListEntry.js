02ca04ef0a43c9e763c3d00014e96dda08575809
Tariq West <tariqwest@gmail.com>
2017-06-01 19:37:40 -0700

    Show additional chat message info

--- client/src/components/ChatsListEntry.js
+++ client/src/components/ChatsListEntry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChatListEntry = props => (
-  <div>message: { props.chatMessage.message }</div>
+  <div>to_id: { props.chatMessage.profile_id_to }, from_id: { props.chatMessage.profile_id_from } message: { props.chatMessage.message } time_stamp: { props.chatMessage.created_at }</div>
 );
 
 export default ChatListEntry;
