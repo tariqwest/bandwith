cfc952246d9c5c4c75e679e3b7c1a310f7896642
April Ablon <ablonapril@gmail.com>
2017-06-01 21:21:57 -0700

    Add matches reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import redirectURL from './redirectURL';
 import chat from './chat';
+import matches from './matches';
 
 // We combine the reducers here
 const rootReducer = combineReducers({
   auth,
   redirectURL,
   chat,
+  matches,
 });
 
 export default rootReducer;
