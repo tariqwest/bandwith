ef7060c016e9b0c90eb87c62ce5cf4587ba8810d
April Ablon <ablonapril@gmail.com>
2017-06-03 10:58:29 -0700

    Add connections promise chain

--- db/seeds/dev_data_seed.js
+++ db/seeds/dev_data_seed.js
@@ -17,18 +17,16 @@ const genres = seedGenres();
 const influences = seedInfluences();
 
 const auth = profiles.then(seedAuth);
-// const connections = profiles.then(seedConnections)
-//   .then(seedChats);
+const connections = profiles.then(seedConnections)
+  .then(seedChats);
 
 const influenceR = Promise.all([profiles, influences])
-  .then(seedInfluenceRelationships); // two tables
+  .then(seedInfluenceRelationships); 
 
 const instrumentR = Promise.all([profiles, instruments])
-  .then(seedInstrumentRelationships); // two tables
+  .then(seedInstrumentRelationships); 
 
 const genreR = Promise.all([profiles, genres])
-  .then(seedGenreRelationships); // two tables
-
-// Promise.all([influences, connections, instrumentR, genreR, auth]); //done
+  .then(seedGenreRelationships); 
 
 exports.seed = (knex, Promise) => Promise.all([profiles, instruments, genres, auth, influenceR]);
