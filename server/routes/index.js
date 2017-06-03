0609c7661ee94a5c604d5bdbdad1bb87c4bb4d25
April Ablon <ablonapril@gmail.com>
2017-06-03 11:14:07 -0700

    Remove line importing test file as it no longer exists

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ module.exports.api = require('./api');
 module.exports.auth = require('./auth');
 module.exports.profiles = require('./profiles');
 module.exports.chats = require('./chats');
-module.exports.test = require('./test');
 module.exports.connections = require('./connections');
 module.exports.preference = require('./preference');
 module.exports.signup = require('./signup');
