654b00fed205e1ae4576cc0744a165bc1a9d2d70
Max Braz <braz.max@gmail.com>
2017-06-02 19:37:23 -0700

    import signup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,7 @@ import auth from './auth';
 import redirectURL from './redirectURL';
 import chat from './chat';
 import matches from './matches';
+import signup from './signup';
 
 // We combine the reducers here
 const rootReducer = combineReducers({
@@ -10,6 +11,7 @@ const rootReducer = combineReducers({
   redirectURL,
   chat,
   matches,
+  signup,
 });
 
 export default rootReducer;
