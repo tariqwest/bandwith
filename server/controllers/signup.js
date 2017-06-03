45b3e4261369581d136171a4226aee201c00e27a
Max Braz <braz.max@gmail.com>
2017-06-03 14:26:11 -0700

    add variable for new db insert

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -13,13 +13,26 @@ module.exports.update = (req, res) => {
     searchRadius: req.body.searchRadius,
   };
 
+  const userInstruments = req.body.instruments;
+
+  const userGenres = req.body.genres;
+
+  const userInfluences = req.body.influences;
+
+  const preferredInstruments = req.body.preferred_instruments;
+
+  const preferredGenres = req.body.preferred_genres;
+
+  // make sure there are no blank fields because table insert will break
   const keys = Object.keys(profileBody);
+
   for (let i = 0; i < keys.length; i++) {
     if (profileBody[keys[i]] === '') {
       delete profileBody[keys[i]];
     }
   }
 
+  // update the user profile table
   models.Profile.where({ id: req.body.id }).fetch()
     .then((profile) => {
       if (!profile) {
