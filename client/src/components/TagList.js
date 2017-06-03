de13df401a5eebc3e8e73f80bdf24459cd34ea21
April Ablon <ablonapril@gmail.com>
2017-06-03 12:29:26 -0700

    Fix syntax

--- client/src/components/TagList.js
+++ client/src/components/TagList.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Tag from './Tag';
 
-const TagList = props => {
-  return (
-    <div>
-      {props.tags.map((tag) =>
-        <Tag tag={tag} type={props.type} key={tag} />
-      )}
-    </div>
-  );
-};
+const TagList = props => (
+  <div>
+    {props.tags.map(tag =>
+      <Tag tag={tag} type={props.type} key={tag} />  // eslint-disable-line
+    )}
+  </div>
+);
 
 export default TagList;
