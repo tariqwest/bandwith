b744e8c68ef011676e9bb87c593997567def6824
Max Braz <braz.max@gmail.com>
2017-06-02 19:36:40 -0700

    add signup

---
+++ client/src/actions/signup.js
@@ -0,0 +1,36 @@
+export const UPDATE_REQUEST = 'UPDATE_REQUEST';
+export const UPDATE_SUCCESS = 'UPDATE_SUCCESS';
+export const UPDATE_FAILURE = 'UPDATE_FAILURE';
+
+export const requestUpdate = () => ({
+  type: UPDATE_REQUEST,
+});
+
+export const receiveUpdate = () => ({
+  type: UPDATE_SUCCESS,
+});
+
+export const updateError = message => ({
+  type: UPDATE_FAILURE,
+  message,
+});
+
+export const updateProfile = profile => (dispatch) => {
+  dispatch(requestUpdate());
+
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(profile),
+    headers,
+  };
+
+  return fetch('/api/signup', options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
