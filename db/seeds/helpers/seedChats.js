9aa3053302820be40707ba46cc074fdb8a6f0bd1
April Ablon <ablonapril@gmail.com>
2017-06-03 10:59:58 -0700

    Complete seedChats functionality

--- db/seeds/helpers/seedChats.js
+++ db/seeds/helpers/seedChats.js
@@ -1,6 +1,18 @@
-exports.seedChats = (connections) => {
-  // array of profiles
-  // create relationships
-  // add to connections table
-  // return connections
-};
+const models = require('../../models');
+
+module.exports = connections => (
+  Promise.all(connections.map(connection =>
+    Promise.all([
+      models.Chat.forge({
+        profile_id_to: connection.attributes.profile_id_1,
+        profile_id_from: connection.attributes.profile_id_2,
+        message: 'hi there',
+      }).save(),
+      models.Chat.forge({
+        profile_id_to: connection.attributes.profile_id_2,
+        profile_id_from: connection.attributes.profile_id_1,
+        message: 'oh hey wassup',
+      }).save(),
+    ])
+  ))
+);
