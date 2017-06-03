01cf07231151ec39c73907e8319271cb0ff910d4
April Ablon <ablonapril@gmail.com>
2017-06-03 11:00:19 -0700

    Complete seedConnections functionality

--- db/seeds/helpers/seedConnections.js
+++ db/seeds/helpers/seedConnections.js
@@ -1,7 +1,26 @@
-exports.seedConnections = (profiles) => {
-  console.log('seed connections PROFILES: ', profiles);
-  // array of profiles
-  // create relationships
-  // add to connections table
-  // return connections
+const models = require('../../models');
+
+module.exports = (profiles) => {
+  const ids = profiles.map(profile => profile.id);
+  const connectionsInput = [
+    {
+      profile_id_1: ids[0],
+      profile_id_2: ids[1],
+      likes_1_2: true,
+      likes_2_1: true,
+    },
+    {
+      profile_id_1: ids[0],
+      profile_id_2: ids[2],
+      likes_1_2: true,
+      likes_2_1: true,
+    },
+    {
+      profile_id_1: ids[0],
+      profile_id_2: ids[3],
+      likes_1_2: true,
+      likes_2_1: true,
+    },
+  ];
+  return Promise.all(connectionsInput.map(input => models.Connection.forge(input).save()));
 };
