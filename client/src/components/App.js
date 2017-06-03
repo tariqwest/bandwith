f6a1919a24e135f73dbf61119a53ca51999f3c46
April Ablon <ablonapril@gmail.com>
2017-06-03 15:39:09 -0700

    Add MuiThemeProvider and injectTapEventPlugin for material-ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,10 @@ import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
 } from 'react-router-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Matches from './Matches';
 import Results from './Results';
@@ -15,31 +17,28 @@ import Private from './Private';
 import Profile from './Profile';
 import PrivateRoute from './PrivateRoute';
 import Signup from './Signup';
+import Nav from './Nav';
 import store from '../store';
+
+injectTapEventPlugin();
 
 const App = () => (
   <Provider store={store}>
-    <Router>
-      <div>
-        <ul>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-          <li><Link to="/private">Protected Page</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/chats">Chats</Link></li>
-          <li><Link to="/connections">Connections</Link></li>
-          <li><Link to="/results">Musicians</Link></li>
-        </ul>
-        <Route path="/login" component={Login} />
-        <Route path="/logout" component={Logout} />
-        <PrivateRoute path="/private" component={Private} />
-        <PrivateRoute path="/profile" component={Profile} />
-        <PrivateRoute path="/connections" component={Matches} />
-        <PrivateRoute path="/results" component={Results} />
-        <PrivateRoute path="/chats" component={Chats} />
-        <Route path="/signup" component={Signup} />
-      </div>
-    </Router>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <Router>
+        <div>
+          <Nav />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <PrivateRoute path="/private" component={Private} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/connections" component={Matches} />
+          <PrivateRoute path="/results" component={Results} />
+          <PrivateRoute path="/chats" component={Chats} />
+          <Route path="/signup" component={Signup} />
+        </div>
+      </Router>
+    </MuiThemeProvider>
   </Provider>
 );
 
