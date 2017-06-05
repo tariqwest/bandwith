e431fc1579f4a283ab3f56c989736026be63e1d5
April Ablon <ablonapril@gmail.com>
2017-06-05 16:29:03 -0700

    Update Nav component to include Material-UI tabs and update App Bar styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,14 +4,34 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import { connect } from 'react-redux';
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { open: false };
+    this.state = { open: false, slideIndex: 0 };
     this.handleToggle = this.handleToggle.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.changeTab = this.changeTab.bind(this);
+  }
+
+  componentWillMount() {
+    if (location.pathname === '/profile') {
+      this.setState({
+        slideIndex: 0,
+      });
+    } else if (location.pathname === '/results') {
+      this.setState({
+        slideIndex: 1,
+      });
+    } else if (location.pathname === '/connections') {
+      this.setState({
+        slideIndex: 2,
+      });
+    }
   }
 
   handleToggle() {
@@ -22,30 +42,67 @@ class Nav extends React.Component {
     this.setState({ open: false });
   }
 
+  changeTab(event) {
+    this.setState({
+      slideIndex: event,
+    });
+  }
+
   render() {
+    const styles = {
+      default_tab:{
+        color: '#6d6d6d',
+      },
+      active_tab:{
+        color: '#fff',
+      },
+    };
+
+    styles.tab = [];
+    styles.tab[0] = styles.default_tab;
+    styles.tab[1] = styles.default_tab;
+    styles.tab[2] = styles.default_tab;
+    styles.tab[this.state.slideIndex] = styles.active_tab;
+
     if (this.props.isAuthenticated) {
       return (
         <div>
           <AppBar
             title={<NavLink exact to="/" >Bandwith</NavLink>}
-            onLeftIconButtonTouchTap={this.handleToggle}
-            style={{ backgroundColor: 'black' }}
-            iconElementRight={<FlatButton
-              label="Logout"
-              containerElement={<Link to="/logout" />}
-            />}
+            onRightIconButtonTouchTap={this.handleToggle}
+            showMenuIconButton={false}
+            style={{ backgroundColor: 'black', fontFamily: 'Pacifico' }}
+            iconElementRight={<IconButton><MoreVertIcon /></IconButton>}
+            iconStyleRight={{ color: 'white' }}
           />
           <Drawer
             docked={false}
             open={this.state.open}
+            openSecondary={true}
             onRequestChange={open => this.setState({ open })}
           >
-            <MenuItem primaryText="Profile" containerElement={<Link to="/profile" />} />
             <MenuItem primaryText="Logout" containerElement={<Link to="/logout" />} />
           </Drawer>
-          <FlatButton label="Profile" containerElement={<Link to="/profile" />} />
-          <FlatButton label="Connections" containerElement={<Link to="/connections" />} />
-          <FlatButton label="Musicians" containerElement={<Link to="/results" />} />
+          <Tabs onChange={this.changeTab} value={this.state.slideIndex} inkBarStyle={{ background: '#ff5bff' }} tabItemContainerStyle={{ background: 'black' }}>
+            <Tab
+              style={styles.tab[0]}
+              value={0}
+              label={<i className="material-icons">account_circle</i>}
+              containerElement={<Link to="/profile" />}
+            />
+            <Tab
+              style={styles.tab[1]}
+              value={1}
+              label={<i className="material-icons">people</i>}
+              containerElement={<Link to="/results" />}
+            />
+            <Tab
+              style={styles.tab[2]}
+              value={2}
+              label={<i className="material-icons">favorite</i>}
+              containerElement={<Link to="/connections" />}
+            />
+          </Tabs>
         </div>
       );
     }
@@ -53,17 +110,10 @@ class Nav extends React.Component {
       <div>
         <AppBar
           title={<NavLink exact to="/" >Bandwith</NavLink>}
-          onLeftIconButtonTouchTap={this.handleToggle}
-          style={{ backgroundColor: 'black' }}
+          showMenuIconButton={false}
+          style={{ backgroundColor: 'black', fontFamily: 'Pacifico' }}
           iconElementRight={<FlatButton label="Login" containerElement={<Link to="/login" />} />}
         />
-        <Drawer
-          docked={false}
-          open={this.state.open}
-          onRequestChange={open => this.setState({ open })}
-        >
-          <MenuItem primaryText="Login" containerElement={<Link to="/login" />} />
-        </Drawer>
       </div>
     );
   }
