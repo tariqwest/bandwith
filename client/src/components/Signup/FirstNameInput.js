465858cb38a3e82b295b9f4765c83e1d253f3f30
Max Braz <braz.max@gmail.com>
2017-06-05 17:12:00 -0700

    adding icons.  edit list

--- client/src/components/Signup/FirstNameInput.js
+++ client/src/components/Signup/FirstNameInput.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import { List, ListItem } from 'material-ui/List';
 
 const FirstNameInput = ({ value, onChange }) => (
-  <div>
-    <TextField
-      floatingLabelText="First Name"
-      required
-      id="first"
-      type="text"
-      name="first"
-      value={value}
-      onChange={onChange}
-    />
-  </div>
+  <List>
+    <ListItem leftIcon={<i className="material-icons" >perm_identity</i>} >
+      <TextField
+        floatingLabelText="First Name"
+        required
+        id="first"
+        type="text"
+        name="first"
+        value={value}
+        onChange={onChange}
+      />
+    </ListItem>
+  </List>
 );
 
 export default FirstNameInput;
