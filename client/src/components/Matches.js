7fe43501854c6da6813331d7ff796d5c6a2ea323
April Ablon <ablonapril@gmail.com>
2017-06-06 11:34:10 -0700

    Add react-flexbox-grid to Matches component

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
+import { Card, CardTitle } from 'material-ui/Card';
+import { Row, Col } from 'react-flexbox-grid';
 import MatchList from './MatchList';
-import { getMatchesInfo } from '../actions';
 
 class Matches extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
     return (
-      <div>
-        <h1>Connections</h1>
-        <MatchList />
-      </div>
+      <Row>
+        <Col xs={0} sm={3} md={3} lg={3} />
+        <Col xs={12} sm={6} md={6} lg={6}>
+          <Card>
+            <CardTitle title="Connections" />
+            <MatchList />
+          </Card>
+        </Col>
+        <Col xs={0} sm={3} md={3} lg={3} />
+      </Row>
     );
   }
 }
