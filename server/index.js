35443710b4a529bdae30aa5d53f404a2c9482303
calebcordry <caleb.cordry@gmail.com>
2017-06-06 15:00:35 -0700

    try without socketio

--- server/index.js
+++ server/index.js
@@ -8,27 +8,27 @@ const ChatController = require('./controllers').Chats;
 
 
 const PORT = process.env.PORT || 3000;
-
-const server = http.Server(app);
-const io = socketio(server.listen(PORT, () => {
-  console.log('Bandwith listening on port 3000!');
-}));
-
-io.use(passportSocketIo.authorize({
-  key: 'connect.sid',
-  secret: 'more laughter, more love, more life',
-  store: middleware.auth.Store,
-  passport: middleware.passport,
-  cookieParser,
-}));
-
-const userToClientMap = {};
-
-io.on('connection', (socket) => {
-  console.log(`A client just joined with socketId: ${socket.id}, userId: ${socket.request.user.id}`);
-  userToClientMap[socket.request.user.id] = socket.id;
-  socket.on('chat', (message) => {
-    console.log(`** message from userId: ${socket.request.user.id}: ${JSON.stringify(message)}`);
-    ChatController.socketCreate(message, socket, userToClientMap);
-  });
-});
+app.listen(PORT);
+// const server = http.Server(app);
+// const io = socketio(server.listen(PORT, () => {
+//   console.log('Bandwith listening on port 3000!');
+// }));
+//
+// io.use(passportSocketIo.authorize({
+//   key: 'connect.sid',
+//   secret: 'more laughter, more love, more life',
+//   store: middleware.auth.Store,
+//   passport: middleware.passport,
+//   cookieParser,
+// }));
+//
+// const userToClientMap = {};
+//
+// io.on('connection', (socket) => {
+//   console.log(`A client just joined with socketId: ${socket.id}, userId: ${socket.request.user.id}`);
+//   userToClientMap[socket.request.user.id] = socket.id;
+//   socket.on('chat', (message) => {
+//     console.log(`** message from userId: ${socket.request.user.id}: ${JSON.stringify(message)}`);
+//     ChatController.socketCreate(message, socket, userToClientMap);
+//   });
+// });
