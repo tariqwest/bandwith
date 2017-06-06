a3f32489ea52125091fff4d93bbd39142caaf1c2
April Ablon <ablonapril@gmail.com>
2017-06-06 11:32:53 -0700

    Refactor ChatsList to use Material-UI

--- client/src/components/ChatsList.js
+++ client/src/components/ChatsList.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import io from 'socket.io-client';
-import ChatsListEntry from './ChatsListEntry';
 import { connect } from 'react-redux';
-import { sendChat, getChats } from '../actions';
+import { CardText } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import { List } from 'material-ui/List';
+import ChatsListEntry from './ChatsListEntry';
+import { getChats } from '../actions';
 
 class ChatsList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { message: '' };
     this.handleSendClick = this.handleSendClick.bind(this);
+    this.saveMessage = this.saveMessage.bind(this);
   }
 
   componentDidMount(){
     const { dispatch } = this.props;
     this.socket = io('http://localhost:3000');
-    this.socket.on('chat', (message) => {
+    this.socket.on('chat', () => {
       dispatch(getChats(this.props.userId, this.props.currentMatchUserId));
     });
     dispatch(getChats(this.props.userId, this.props.currentMatchUserId));
+  }
+
+  saveMessage(event) {
+    this.setState({
+      message: event.target.value,
+    });
+  }
+
+  handleSendClick() {
+    const { dispatch } = this.props;
+    const message = this.state.message;
+    this.socket.emit('chat', { userId: this.props.userId, matchUserId: this.props.currentMatchUserId, message });
+    dispatch(getChats(this.props.userId, this.props.currentMatchUserId));
+    this.setState({
+      message: '',
+    });
   }
 
   render() {
@@ -24,33 +46,33 @@ class ChatsList extends React.Component {
       if (this.props.currentMatchChatMessages.length > 0) {
         return (
           this.props.currentMatchChatMessages
-          .map((chatMessage, i) => {
-            return <ChatsListEntry key={i} chatMessage={chatMessage} />;
-          })
+          .map((chatMessage, i) => (
+            <ChatsListEntry key={i} chatMessage={chatMessage} />
+          ))
         );
-      } else {
-        return (<div>No messages form this user. Start a conversation!</div>);
       }
+      return (<CardText>No messages form this user. Start a conversation!</CardText>);
     };
 
     return (
       <div>
-        <div>
-          <input placeholder="Enter your message..." id="message" type="text" />
-          <button onClick={this.handleSendClick}>Send</button>
-        </div>
-        {chatMessages()}
+        <CardText>
+          <TextField
+            id="message"
+            type="text"
+            floatingLabelShrinkStyle={{ color: '#000' }}
+            underlineFocusStyle={{ borderColor: '#000' }}
+            value={this.state.message}
+            onChange={this.saveMessage}
+            floatingLabelText="Enter your message..."
+          />
+          <FlatButton label="Send" onClick={this.handleSendClick} />
+        </CardText>
+        <List>
+          {chatMessages()}
+        </List>
       </div>
     );
-  }
-
-  handleSendClick(event) {
-    const { dispatch } = this.props;
-    const message = document.getElementById('message').value;
-    this.socket.emit('chat', { userId: this.props.userId, matchUserId: this.props.currentMatchUserId, message });
-    // dispatch(sendChat(this.props.userId, this.props.currentMatchUserId, message));
-    dispatch(getChats(this.props.userId, this.props.currentMatchUserId));
-    document.getElementById('message').value = '';
   }
 }
 
