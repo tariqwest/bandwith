185a7a4bb8790e7fdcec09c4c615ebda0e54d6f3
calebcordry <caleb.cordry@gmail.com>
2017-06-06 14:54:03 -0700

    add log in middleware

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ChatController = require('../controllers').Chats
+const ChatController = require('../controllers').Chats;
 
 const router = express.Router();
 
