ef4f4f6fca90028305bd3adb4d886c8f783c4fc1
April Ablon <ablonapril@gmail.com>
2017-06-07 11:09:43 -0700

    Refactor component to grab location data from redux store rather than locally

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import FlatButton from 'material-ui/FlatButton';
 import { Card,
   CardHeader,
@@ -10,37 +9,13 @@ import { Card,
 } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
 import { Row, Col } from 'react-flexbox-grid';
-import axios from 'axios';
 import FullscreenDialog from 'material-ui-fullscreen-dialog';
 import Signup from './Signup';
-import { setUserLocation } from '../actions';
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      city: '',
-      county: '',
-      lat: 0.0,
-      lng: 0.0,
-      state: '',
-      showEditProfile: false,
-      state_abbrev: '',
-      zipcode: 0,
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    axios.get(`/api/location/${nextProps.user.zipCode}`)
-      .then((response) => {
-        const location = response.data;
-        this.setState(location);
-        const { dispatch } = this.props;
-        dispatch(setUserLocation(location));
-      })
-      .catch((err) => {
-        throw err;
-      });
+    this.state = { showEditProfile: false };
   }
 
   render() {
@@ -66,124 +41,127 @@ class Profile extends React.Component {
     const fullname = first + ' ' + last;
     const search = 'Searching within ' + searchRadius + ' miles';
     const profile = gender + ', ' + age;
-    const location = this.state.city + ', ' + this.state.state + ' ' + zipCode;
+    const location = this.props.location.city + ', ' + this.props.location.state + ' ' + zipCode;
 
     if (this.props.hasInfo) {
       return (
         <div>
-        <Row>
-          <Col xs={0} sm={3} md={3} lg={3} />
-          <Col xs={12} sm={6} md={6} lg={6}>
-            <Card>
-              <CardHeader avatar={photo_src} title={fullname} subtitle={display} />
-              <CardTitle title={fullname} subtitle={bio} />
-              <List>
-                <ListItem
-                  leftIcon={<i className="material-icons">account_circle</i>}
-                  primaryText={profile}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">email</i>}
-                  primaryText={email}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">place</i>}
-                  primaryText={location}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">near_me</i>}
-                  primaryText={search}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">speaker</i>}
-                  primaryText="My Instruments"
-                  primaryTogglesNestedList={true}
-                  nestedItems={instruments.map(instrument =>
-                    <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
-                  )}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">album</i>}
-                  primaryText="My Genres"
-                  primaryTogglesNestedList={true}
-                  nestedItems={genres.map(genre =>
-                    <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
-                  )}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">headset</i>}
-                  primaryText="My Influences"
-                  primaryTogglesNestedList={true}
-                  nestedItems={influences.map(influence =>
-                    <ListItem key={influence} primaryText={influence} /> // eslint-disable-line
-                  )}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">grade</i>}
-                  primaryText="Preferred Instruments"
-                  primaryTogglesNestedList={true}
-                  nestedItems={preferredInstruments.map(instrument =>
-                    <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
-                  )}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">favorite</i>}
-                  primaryText="Preferred Genres"
-                  primaryTogglesNestedList={true}
-                  nestedItems={preferredGenres.map(genre =>
-                    <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
-                  )}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">music_video</i>}
-                  primaryText="YouTube"
-                  primaryTogglesNestedList={true}
-                  nestedItems={[
-                    <CardMedia key={video_url}>
-                      <iframe
-                        frameBorder="0"
-                        allowFullScreen
-                        title="video"
-                        src={video_url}
-                      />
-                    </CardMedia>,
-                  ]}
-                />
-                <ListItem
-                  leftIcon={<i className="material-icons">audiotrack</i>}
-                  primaryText="SoundCloud"
-                  primaryTogglesNestedList={true}
-                  nestedItems={[
-                    <CardMedia key={song_url}>
-                      <iframe
-                        scrolling="no"
-                        frameBorder="no"
-                        title="audio"
-                        src={song_url}
-                      />
-                    </CardMedia>,
-                  ]}
-                />
-              </List>
-              <CardActions>
-                <FlatButton label="Edit" onClick={() => this.setState({ showEditProfile: true })} />
-              </CardActions>
-            </Card>
-          </Col>
-          <Col xs={0} sm={3} md={3} lg={3} />
-        </Row>
+          <Row>
+            <Col xs={0} sm={3} md={3} lg={3} />
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <Card>
+                <CardHeader avatar={photo_src} title={fullname} subtitle={display} />
+                <CardTitle title={fullname} subtitle={bio} />
+                <List>
+                  <ListItem
+                    leftIcon={<i className="material-icons">account_circle</i>}
+                    primaryText={profile}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">email</i>}
+                    primaryText={email}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">place</i>}
+                    primaryText={location}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">near_me</i>}
+                    primaryText={search}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">speaker</i>}
+                    primaryText="My Instruments"
+                    primaryTogglesNestedList={true}
+                    nestedItems={instruments.map(instrument =>
+                      <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
+                    )}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">album</i>}
+                    primaryText="My Genres"
+                    primaryTogglesNestedList={true}
+                    nestedItems={genres.map(genre =>
+                      <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
+                    )}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">headset</i>}
+                    primaryText="My Influences"
+                    primaryTogglesNestedList={true}
+                    nestedItems={influences.map(influence =>
+                      <ListItem key={influence} primaryText={influence} /> // eslint-disable-line
+                    )}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">grade</i>}
+                    primaryText="Preferred Instruments"
+                    primaryTogglesNestedList={true}
+                    nestedItems={preferredInstruments.map(instrument =>
+                      <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
+                    )}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">favorite</i>}
+                    primaryText="Preferred Genres"
+                    primaryTogglesNestedList={true}
+                    nestedItems={preferredGenres.map(genre =>
+                      <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
+                    )}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">music_video</i>}
+                    primaryText="YouTube"
+                    primaryTogglesNestedList={true}
+                    nestedItems={[
+                      <CardMedia key={video_url}>
+                        <iframe
+                          frameBorder="0"
+                          allowFullScreen
+                          title="video"
+                          src={video_url}
+                        />
+                      </CardMedia>,
+                    ]}
+                  />
+                  <ListItem
+                    leftIcon={<i className="material-icons">audiotrack</i>}
+                    primaryText="SoundCloud"
+                    primaryTogglesNestedList={true}
+                    nestedItems={[
+                      <CardMedia key={song_url}>
+                        <iframe
+                          scrolling="no"
+                          frameBorder="no"
+                          title="audio"
+                          src={song_url}
+                        />
+                      </CardMedia>,
+                    ]}
+                  />
+                </List>
+                <CardActions>
+                  <FlatButton
+                    label="Edit"
+                    onClick={() => this.setState({ showEditProfile: true })}
+                  />
+                </CardActions>
+              </Card>
+            </Col>
+            <Col xs={0} sm={3} md={3} lg={3} />
+          </Row>
 
           <FullscreenDialog
-              open={this.state.showEditProfile}
-              onRequestClose={() => this.setState({ showEditProfile: false })}
-              title={this.props.user.display}
-              actionButton={<FlatButton
-                label='Close'
-                onTouchTap={() => this.setState({ showEditProfile: false })}
-              />}
-              appBarStyle={{backgroundColor: '#000000'}}
-            >
-              <Signup />
+            open={this.state.showEditProfile}
+            onRequestClose={() => this.setState({ showEditProfile: false })}
+            title={this.props.user.display}
+            actionButton={<FlatButton
+              label="Close"
+              onTouchTap={() => this.setState({ showEditProfile: false })}
+            />}
+            appBarStyle={{ backgroundColor: '#000000' }}
+          >
+            <Signup />
           </FullscreenDialog>
         </div>
       );
@@ -197,6 +175,7 @@ class Profile extends React.Component {
 const mapStateToProps = state => ({
   user: state.user.profile,
   hasInfo: state.user.hasInfo,
+  location: state.location,
 });
 
 export default connect(mapStateToProps)(Profile);
