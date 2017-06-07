f43345ea9797cb3f2e2ef195b5c1b427ed05dcd6
April Ablon <ablonapril@gmail.com>
2017-06-07 12:12:07 -0700

    Filter search results based on search radius and location

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -34,8 +34,32 @@ const profileMatches = (profile) => {
   return instrumentPrefsMatch && genrePrefsMatch;
 };
 
-const addLocationInfo = (profile) => {
-  
+const deg2rad = deg => (deg * (Math.PI/180));
+
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    (Math.sin(dLat / 2) * Math.sin(dLat / 2)) +
+    (Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2))
+    ;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  const d = R * c; // Distance in km
+  return d * 0.62137119; // convert to miles
+};
+
+const locationMatches = (profile) => {
+  const minSearchRadius = Math.min(profile.searchRadius, currentUserProfile.searchRadius);
+
+  const distance = getDistanceFromLatLonInKm(
+    currentUserProfile.lat,
+    currentUserProfile.lng,
+    profile.lat,
+    profile.lng);
+
+  return distance <= minSearchRadius;
 };
 
 module.exports.search = (req, res) => {
@@ -47,14 +71,29 @@ module.exports.search = (req, res) => {
       'genres',
       'preferred_genres',
     ],
-  }).then((profile) => {
+  })
+  .then((profile) => {
     if (!profile) {
       throw profile;
     }
     currentUserProfile = processProfileRelations(profile);
+    return currentUserProfile;
   })
-  .then(() => {
-    return models.Profile.fetchAll({
+  .then((profile) => {
+    models.Location.where({ zipcode: profile.zipCode }).fetch()
+      .then((result) => {
+        const location = {};
+        for (var prop in result.attributes) {
+          if (prop !== 'id' && prop !== 'zipcode') {
+            location[prop] = result.attributes[prop];
+          }
+        }
+        currentUserProfile = Object.assign(profile, location);
+        console.log('currentUserProfile: ', currentUserProfile);
+      });
+  })
+  .then(() => (
+    models.Profile.fetchAll({
       withRelated: [
         'influences',
         'instruments',
@@ -62,8 +101,8 @@ module.exports.search = (req, res) => {
         'genres',
         'preferred_genres',
       ],
-    });
-  })
+    })
+  ))
   .then((profiles) => {
     if (!profiles) {
       throw profiles;
@@ -122,12 +161,26 @@ module.exports.search = (req, res) => {
     return currentMatchedProfiles;
   })
   .then((connections) => {
-    console.log('aprils connections: ', connections);
-    // remove self if included in list
-    // get location data for each - helper function?
-    // get search radius
-    return connections;
+    const connectionsWithLocation = connections.map((connection) => {
+      return models.Location.where({ zipcode: connection.zipCode }).fetch()
+        .then((result) => {
+          const location = {};
+          for (var prop in result.attributes) {
+            if (prop !== 'id' && prop !== 'zipcode') {
+              location[prop] = result.attributes[prop];
+            }
+          }
+          return Object.assign(connection, location);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    });
+    return Promise.all(connectionsWithLocation);
   })
+  .then(connections => (
+    connections.filter(connection => locationMatches(connection))
+  ))
   .then((results) => {
     if (!results) {
       throw results;
