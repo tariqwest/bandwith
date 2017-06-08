6b3e09c972599cd1abcfb593d24eb3e167a0efe7
Max Braz <braz.max@gmail.com>
2017-06-08 15:59:05 -0700

    renamed

---
+++ client/src/components/Signup/DropZone.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class DropZone extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        DropZone
+      </div>
+    );
+  }
+}
+
+export default DropZone;
