67728a221b549afe727bae5fbd8d1947cc09a207
April Ablon <ablonapril@gmail.com>
2017-06-07 17:24:19 -0700

    Update styling to be multiple cards rather than one list

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import FlatButton from 'material-ui/FlatButton';
 import { Card,
-  CardHeader,
   CardTitle,
   CardMedia,
   CardActions,
 } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
+import Paper from 'material-ui/Paper';
+import Chip from 'material-ui/Chip';
 import { Row, Col } from 'react-flexbox-grid';
 import FullscreenDialog from 'material-ui-fullscreen-dialog';
 import Signup from './Signup';
+
+const style = {
+  marginTop: 15,
+};
+const chipStyle = {
+  marginTop: 5,
+  marginLeft: 5,
+  display: 'inline-block',
+};
 
 class Profile extends React.Component {
   constructor(props) {
@@ -38,117 +48,166 @@ class Profile extends React.Component {
       song_url,
       photo_src,
     } = this.props.user;
-    const fullname = first + ' ' + last;
-    const search = 'Searching within ' + searchRadius + ' miles';
-    const profile = gender + ', ' + age;
-    const location = this.props.location.city + ', ' + this.props.location.state + ' ' + zipCode;
+    const { city, state } = this.props.location;
+    const fullname = `${first} ${last}`;
+    const search = `Searching within ${searchRadius} miles`;
+    const profile = `${gender}, ${age}`;
+    const location = `${city}, ${state} ${zipCode}`;
 
     if (this.props.hasInfo) {
       return (
         <div>
           <Row>
-            <Col xs={0} sm={3} md={3} lg={3} />
-            <Col xs={12} sm={6} md={6} lg={6}>
-              <Card>
-                <CardHeader avatar={photo_src} title={fullname} subtitle={display} />
-                <CardTitle title={fullname} subtitle={bio} />
-                <List>
-                  <ListItem
-                    leftIcon={<i className="material-icons">account_circle</i>}
-                    primaryText={profile}
+            <Col xs={12} sm={8} xsOffset={0} smOffset={2}>
+              <Paper style={style}>
+                <Card className="chat-title">
+                  <img
+                    className="chat-picture"
+                    width="100"
+                    height="100"
+                    alt="profile-pic"
+                    src={photo_src}
                   />
-                  <ListItem
-                    leftIcon={<i className="material-icons">email</i>}
-                    primaryText={email}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">place</i>}
-                    primaryText={location}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">near_me</i>}
-                    primaryText={search}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">speaker</i>}
-                    primaryText="My Instruments"
-                    primaryTogglesNestedList={true}
-                    nestedItems={instruments.map(instrument =>
-                      <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
-                    )}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">album</i>}
-                    primaryText="My Genres"
-                    primaryTogglesNestedList={true}
-                    nestedItems={genres.map(genre =>
-                      <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
-                    )}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">headset</i>}
-                    primaryText="My Influences"
-                    primaryTogglesNestedList={true}
-                    nestedItems={influences.map(influence =>
-                      <ListItem key={influence} primaryText={influence} /> // eslint-disable-line
-                    )}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">grade</i>}
-                    primaryText="Preferred Instruments"
-                    primaryTogglesNestedList={true}
-                    nestedItems={preferredInstruments.map(instrument =>
-                      <ListItem key={instrument} primaryText={instrument} /> // eslint-disable-line
-                    )}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">favorite</i>}
-                    primaryText="Preferred Genres"
-                    primaryTogglesNestedList={true}
-                    nestedItems={preferredGenres.map(genre =>
-                      <ListItem key={genre} primaryText={genre} /> // eslint-disable-line
-                    )}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">music_video</i>}
-                    primaryText="YouTube"
-                    primaryTogglesNestedList={true}
-                    nestedItems={[
-                      <CardMedia key={video_url}>
-                        <iframe
-                          frameBorder="0"
-                          allowFullScreen
-                          title="video"
-                          src={video_url}
-                        />
-                      </CardMedia>,
-                    ]}
-                  />
-                  <ListItem
-                    leftIcon={<i className="material-icons">audiotrack</i>}
-                    primaryText="SoundCloud"
-                    primaryTogglesNestedList={true}
-                    nestedItems={[
-                      <CardMedia key={song_url}>
-                        <iframe
-                          scrolling="no"
-                          frameBorder="no"
-                          title="audio"
-                          src={song_url}
-                        />
-                      </CardMedia>,
-                    ]}
-                  />
-                </List>
-                <CardActions>
-                  <FlatButton
-                    label="Edit"
-                    onClick={() => this.setState({ showEditProfile: true })}
-                  />
-                </CardActions>
-              </Card>
+                  <CardTitle title={fullname} subtitle={bio} />
+                </Card>
+              </Paper>
             </Col>
-            <Col xs={0} sm={3} md={3} lg={3} />
+          </Row>
+          <Row>
+            <Col xs={12} sm={4} smOffset={2}>
+              <Paper style={style}>
+                <Card>
+                  <List>
+                    <ListItem
+                      leftIcon={<i className="material-icons">account_circle</i>}
+                      primaryText={profile}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">email</i>}
+                      primaryText={email}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">place</i>}
+                      primaryText={location}
+                    />
+                  </List>
+                </Card>
+              </Paper>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Paper style={style}>
+                <Card>
+                  <List>
+                    <ListItem
+                      leftIcon={<i className="material-icons">music_video</i>}
+                      primaryText="YouTube"
+                      primaryTogglesNestedList={true}
+                      nestedItems={[
+                        <CardMedia key={video_url}>
+                          <iframe
+                            frameBorder="0"
+                            allowFullScreen
+                            title="video"
+                            src={video_url}
+                          />
+                        </CardMedia>,
+                      ]}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">audiotrack</i>}
+                      primaryText="SoundCloud"
+                      primaryTogglesNestedList={true}
+                      nestedItems={[
+                        <CardMedia key={song_url}>
+                          <iframe
+                            scrolling="no"
+                            frameBorder="no"
+                            title="audio"
+                            src={song_url}
+                          />
+                        </CardMedia>,
+                      ]}
+                    />
+                  </List>
+                </Card>
+              </Paper>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={4} smOffset={2}>
+              <Paper style={style}>
+                <Card>
+                  <List>
+                    <ListItem
+                      leftIcon={<i className="material-icons">speaker</i>}
+                      primaryText="My Instruments"
+                      primaryTogglesNestedList={true}
+                      nestedItems={instruments.map(instrument =>
+                        <Chip style={chipStyle}>{instrument}</Chip> // eslint-disable-line
+                      )}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">album</i>}
+                      primaryText="My Genres"
+                      primaryTogglesNestedList={true}
+                      nestedItems={genres.map(genre =>
+                        <Chip style={chipStyle}>{genre}</Chip> // eslint-disable-line
+                      )}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">headset</i>}
+                      primaryText="My Influences"
+                      primaryTogglesNestedList={true}
+                      nestedItems={influences.map(influence =>
+                        <Chip style={chipStyle}>{influence}</Chip> // eslint-disable-line
+                      )}
+                    />
+                  </List>
+                </Card>
+              </Paper>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Paper style={style}>
+                <Card>
+                  <List>
+                    <ListItem
+                      leftIcon={<i className="material-icons">near_me</i>}
+                      primaryText={search}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">grade</i>}
+                      primaryText="Preferred Instruments"
+                      primaryTogglesNestedList={true}
+                      nestedItems={preferredInstruments.map(instrument =>
+                        <Chip style={chipStyle}>{instrument}</Chip> // eslint-disable-line
+                      )}
+                    />
+                    <ListItem
+                      leftIcon={<i className="material-icons">favorite</i>}
+                      primaryText="Preferred Genres"
+                      primaryTogglesNestedList={true}
+                      nestedItems={preferredGenres.map(genre =>
+                        <Chip style={chipStyle}>{genre}</Chip> // eslint-disable-line
+                      )}
+                    />
+                  </List>
+                </Card>
+              </Paper>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Paper style={style}>
+                <Card>
+                  <CardActions>
+                    <FlatButton
+                      label="Edit"
+                      onClick={() => this.setState({ showEditProfile: true })}
+                    />
+                  </CardActions>
+                </Card>
+              </Paper>
+            </Col>
           </Row>
 
           <FullscreenDialog
