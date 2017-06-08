48e5c55a71c135b4f7771c32460adaebc1643883
Tariq West <tariqwest@gmail.com>
2017-06-07 20:50:40 -0700

    Update search logic to filter out already actioned results

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -3,25 +3,40 @@ const db = require('../../db');
 
 const findMatchQuery = userId =>
   `
-  SELECT DISTINCT ID FROM (SELECT
-  PROFILES.ID,
-  PROFILES.DISPLAY,
-  PREFERRED_GENRES.GENRE_ID AS PREFERRED_GENRES_ID,
-  PREFERRED_INSTRUMENTS.INSTRUMENT_ID AS PREFERRED_INSTRUMENTS_ID,
-  USERS_INSTRUMENTS.INSTRUMENT_ID AS USER_INSTRUMENTS_ID,
-  USERS_GENRES.GENRE_ID AS USER_GENRE_ID
-  FROM PROFILES
-  INNER JOIN PREFERRED_GENRES ON PROFILES.ID = PREFERRED_GENRES.PROFILE_ID
-  INNER JOIN PREFERRED_INSTRUMENTS ON PROFILES.ID = PREFERRED_INSTRUMENTS.PROFILE_ID
-  INNER JOIN USERS_GENRES ON  PROFILES.ID = USERS_GENRES.PROFILE_ID
-  INNER JOIN USERS_INSTRUMENTS ON  PROFILES.ID = USERS_INSTRUMENTS.PROFILE_ID
-  WHERE USERS_INSTRUMENTS.INSTRUMENT_ID IN (SELECT INSTRUMENT_ID FROM PREFERRED_INSTRUMENTS WHERE PROFILE_ID = ${userId})
-  AND USERS_GENRES.GENRE_ID IN (SELECT GENRE_ID FROM PREFERRED_GENRES WHERE PROFILE_ID = ${userId}))
-  AS PROFILE_MATCHES `
+  SELECT DISTINCT ID FROM
+    (SELECT
+      PROFILES.ID,
+      PROFILES.DISPLAY,
+      PREFERRED_GENRES.GENRE_ID AS PREFERRED_GENRES_ID,
+      PREFERRED_INSTRUMENTS.INSTRUMENT_ID AS PREFERRED_INSTRUMENTS_ID,
+      USERS_INSTRUMENTS.INSTRUMENT_ID AS USER_INSTRUMENTS_ID,
+      USERS_GENRES.GENRE_ID AS USER_GENRE_ID
+    FROM PROFILES
+      INNER JOIN PREFERRED_GENRES ON PROFILES.ID = PREFERRED_GENRES.PROFILE_ID
+      INNER JOIN PREFERRED_INSTRUMENTS ON PROFILES.ID = PREFERRED_INSTRUMENTS.PROFILE_ID
+      INNER JOIN USERS_GENRES ON  PROFILES.ID = USERS_GENRES.PROFILE_ID
+      INNER JOIN USERS_INSTRUMENTS ON  PROFILES.ID = USERS_INSTRUMENTS.PROFILE_ID
+    WHERE USERS_INSTRUMENTS.INSTRUMENT_ID IN (SELECT INSTRUMENT_ID FROM PREFERRED_INSTRUMENTS WHERE PROFILE_ID = ${userId})
+    AND USERS_GENRES.GENRE_ID IN (SELECT GENRE_ID FROM PREFERRED_GENRES WHERE PROFILE_ID = ${userId}))
+  AS PROFILE_MATCHES
+  `
+;
+
+const findConnectionsQuery = (userId, matchIds) =>
+  `
+  SELECT *
+  FROM
+    CONNECTIONS
+  WHERE
+    (profile_id_1 = 1
+    AND profile_id_2 IN (${matchIds.toString()}))
+  OR
+    (profile_id_2 = 1
+    AND profile_id_1 IN (${matchIds.toString()}))
+  `
 ;
 
 const processProfileRelations = (profile) => {
-  console.log('** Profile: ', profile);
   const influences = profile.related('influences').map(i => i.attributes.influence_name);
   const instruments = profile.related('instruments').map(i => i.attributes.instrument_name);
   const preferredInstruments = profile.related('preferred_instruments').map(p => p.attributes.instrument_name);
@@ -117,30 +132,37 @@ module.exports.search = (req, res) => {
 =======
 module.exports.search = (req, res) => {
   let potentialMatches;
+  let mutualMatches;
   db.knex.raw(findMatchQuery(req.query.userId))
   .then((results) => {
     if (!results) {
       throw results;
     }
-    potentialMatches = results.rows.map((row) => {
-      return row.id;
-    });
-    const matchesOfAllMatches = potentialMatches.map((userId) => {
-      return db.knex.raw(findMatchQuery(userId));
-    });
+    potentialMatches = results.rows.map(row => row.id);
+    const matchesOfAllMatches = potentialMatches.map(userId =>
+      db.knex.raw(findMatchQuery(userId)));
     return Promise.all(matchesOfAllMatches);
   })
   .then((matchesOfAllMatches) => {
     if (!matchesOfAllMatches) {
       throw matchesOfAllMatches;
     }
-    const mutualMatches = [];
+    mutualMatches = [];
     matchesOfAllMatches.forEach((match, i) => {
-      const matchesOfMatch = match.rows.map((match) => {
-        return match.id;
-      });
+      const matchesOfMatch = match.rows.map(matchOfMatch => matchOfMatch.id);
       if (matchesOfMatch.includes(Number(req.query.userId))) {
         mutualMatches.push(potentialMatches[i]);
+      }
+    });
+    return db.knex.raw(findConnectionsQuery(req.query.userId, mutualMatches));
+  })
+  .then((connections) => {
+    connections.rows.forEach((connection) => {
+      if (connection.profile_id_1 === Number(req.query.userId) && connection.likes_1_2 !== null) {
+        mutualMatches = mutualMatches.filter(matchId => matchId !== connection.profile_id_2);
+      }
+      if (connection.profile_id_2 === Number(req.query.userId) && connection.likes_2_1 !== null) {
+        mutualMatches = mutualMatches.filter(matchId => matchId !== connection.profile_id_1);
       }
     });
     return models.Profile.where('id', 'IN', mutualMatches).fetchAll({
