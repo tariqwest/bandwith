bf0f87856af74a7144df67f8492ea2ddea735a3d
calebcordry <caleb.cordry@gmail.com>
2017-06-07 17:22:06 -0700

    refactor webpack for production

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/'),
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        include: [path.join(__dirname, 'client/src'), /flexboxgrid/],
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/client/src/index.html'),
+      favicon: path.join(__dirname, '/client/src/favicon.ico'),
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') },
+    }),
+  ],
+};
