b3d830ea8edb077bfc11b790bfb38c1337267176
Tariq West <tariqwest@gmail.com>
2017-06-08 13:05:41 -0700

    Update seeds with zipcodes and to increase seeded prefs per user

--- db/seeds/helpers/seedInstrumentRelationships.js
+++ db/seeds/helpers/seedInstrumentRelationships.js
@@ -3,7 +3,7 @@ const { randomChoices } = require('./help');
 module.exports = ([profiles, instruments]) =>
   Promise.all(profiles.map(profile => (
     Promise.all([
-      profile.instruments().attach(randomChoices(instruments, 2)),
-      profile.preferred_instruments().attach(randomChoices(instruments, 2)),
+      profile.instruments().attach(randomChoices(instruments, 5)),
+      profile.preferred_instruments().attach(randomChoices(instruments, 5)),
     ]))  // eslint-disable-line
   ));
