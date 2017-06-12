693e0d2951732aa230c1e4b58b26082681e73e56
April Ablon <ablonapril@gmail.com>
2017-06-12 16:42:06 -0700

    Update image upload function to allow image repositioning within display img bounds

--- client/src/components/Signup/ProfileImage.js
+++ client/src/components/Signup/ProfileImage.js
@@ -1,9 +1,12 @@
+/* eslint-disable camelcase */
 import React from 'react';
-import Badge from 'material-ui/Badge';
+import Dialog from 'material-ui/Dialog';
 import { connect } from 'react-redux';
 import request from 'superagent';
-import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Dropzone from 'react-dropzone';
+import Cropper from 'react-cropper';
 
 const style = {
   parent: {
@@ -23,7 +26,18 @@ const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/hjwsarwnv/image/u
 class ProfileImage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cloudinaryUrlLrg: '',
+      cloudinaryUrlSml: '',
+      currentPhoto: this.props.user.photo_src_small,
+      largeImage: this.props.user.photo_src_large,
+      croppedPhoto: '',
+      showEditPhoto: false,
+    };
     this.onDrop = this.onDrop.bind(this);
+    this.closeDropZone = this.closeDropZone.bind(this);
+    this.imageCrop = this.imageCrop.bind(this);
+    this.savePhoto = this.savePhoto.bind(this);
   }
 
   onDrop(photo) {
@@ -38,47 +52,100 @@ class ProfileImage extends React.Component {
 
       if (response.body.secure_url !== '') {
         this.setState({
-          cloudinaryUrl: response.body.secure_url,
+          cloudinaryUrlLrg: response.body.secure_url,
+          largeImage: response.body.secure_url,
+        });
+      }
+    });
+  }
+
+  closeDropZone() {
+    this.setState({
+      showEditPhoto: false,
+    });
+  }
+
+  imageCrop() {
+    this.setState({
+      croppedPhoto: this.refs.cropper.getCroppedCanvas().toDataURL(),
+    });
+  }
+
+  savePhoto() {
+    const upload = request.post(CLOUDINARY_UPLOAD_URL)
+      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+      .field('file', this.state.croppedPhoto);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          cloudinaryUrlSml: response.body.secure_url,
+          currentPhoto: response.body.secure_url,
+          showEditPhoto: false,
         });
 
-        this.props.handlePhotoChange(this.state.cloudinaryUrl);
+        this.props.handlePhotoChange(this.state.cloudinaryUrlLrg, this.state.cloudinaryUrlSml);
       }
     });
   }
 
   render() {
-    const { photo_src } = this.props.user;
     return (
-      <div style={style.parent}>
+      <div>
         <img
           className="chat-picture"
           width="100"
           height="100"
           alt="upload pic"
-          src={photo_src || '/assets/avatar.jpg'}
+          src={this.state.currentPhoto || '/assets/avatar.jpg'}
         />
-        <div style={style.child}>
-          <Dropzone
-            style={style}
-            multiple={false}
-            accept="image/*"
-            onDrop={this.onDrop}
-          >
-            <Badge
-              badgeContent={
-                <IconButton
-                  tooltip="Update Photo"
-                  touch
-                  tooltipPosition="bottom-right"
-                >
-                  <i
-                    className="material-icons"
-                  >mode_edit</i>
-                </IconButton>
-              }
-            />
-          </Dropzone>
-        </div>
+        <FloatingActionButton
+          style={{ marginLeft: '-30px' }}
+          zDepth={1}
+          className="edit-button"
+          onClick={() => this.setState({ showEditPhoto: true })}
+          mini
+          backgroundColor="white"
+          iconStyle={{ color: 'black' }}
+        >
+          <i className="material-icons">create</i>
+        </FloatingActionButton>
+
+        <Dialog
+          open={this.state.showEditPhoto}
+          onRequestClose={() => this.setState({ showEditPhoto: false })}
+          title="Update Profile Picture"
+          actions={[
+            <FlatButton
+              label="Close"
+              onTouchTap={() => this.setState({ showEditPhoto: false })}
+            />,
+            <Dropzone
+              style={style}
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onDrop}
+            >
+              <FlatButton label="Upload Image" />
+            </Dropzone>,
+            <FlatButton
+              label="Save"
+              onTouchTap={this.savePhoto}
+            />,
+          ]}
+        >
+          <Cropper
+            ref="cropper"
+            src={this.state.largeImage}
+            style={{ height: '400px', width: '100%' }}
+            aspectRatio={16 / 16}
+            crop={this.imageCrop}
+          />
+        </Dialog>
       </div>
     );
   }
