3b0724c3b635a90132b535b0f7c0198921819ba0
April Ablon <ablonapril@gmail.com>
2017-06-12 19:22:57 -0700

    Remove location seed functions

--- db/seeds/location_seed.js
+++
@@ -1,6 +0,0 @@
-const locations = require('./lib/zipcodes');
-const models = require('../models');
-
-exports.seed = (knex, Promise) => (
-  Promise.all(locations.map(location => models.Location.forge(location).save()))
-);
