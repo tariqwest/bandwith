ce5ded975949bdf18060c87479ab11b949a569cc
Max Braz <braz.max@gmail.com>
2017-06-14 12:21:44 -0700

    spinner for chats

--- client/src/components/ChatsList.js
+++ client/src/components/ChatsList.js
@@ -3,14 +3,17 @@ import io from 'socket.io-client';
 import { connect } from 'react-redux';
 import { Row, Col } from 'react-flexbox-grid';
 import { CardText, CardActions } from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import { List } from 'material-ui/List';
 import ChatsListEntry from './ChatsListEntry';
+import LoadingSpinner from './LoadingSpinner';
 import { getChats, addSentChat, addReceivedChat } from '../actions';
 
 const styles = {
-
+  paper: { margin: 10 },
+  title: { textAlign: 'center' },
 };
 
 class ChatsList extends React.Component {
@@ -25,6 +28,22 @@ class ChatsList extends React.Component {
 
   render() {
     const chatMessages = () => {
+      if (this.props.isFetching) {
+        return (
+          <div>
+            <div className="bump-tab-bar" />
+            <Row>
+              <Col xs={12} sm={6} smOffset={3}>
+                <Paper style={styles.paper} zDepth={1}>
+                  <div style={styles.title}>
+                    <LoadingSpinner />
+                  </div>
+                </Paper>
+              </Col>
+            </Row>
+          </div>
+        );
+      }
       if (this.props.currentMatchChatMessages.length > 0) {
         return (
           this.props.currentMatchChatMessages
@@ -54,6 +73,7 @@ const mapStateToProps = state => ({
   currentMatchChatMessages: state.chat.currentMatchChatMessages,
   userId: state.auth.userId,
   currentMatchUserId: state.chat.currentMatchUserId,
+  isFetching: state.chat.isFetching,
 });
 
 export default connect(mapStateToProps)(ChatsList);
--- client/src/components/ChatsListEntry.js
+++ client/src/components/ChatsListEntry.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Row, Col } from 'react-flexbox-grid';
+import Paper from 'material-ui/Paper';
 import { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import LoadingSpinner from './LoadingSpinner';
 import moment from 'moment';
+
+const styles = {
+  paper: { margin: 10 },
+  title: { textAlign: 'center' },
+};
 
 class ChatListEntry extends React.Component {
   constructor(props) {
@@ -12,7 +20,23 @@ class ChatListEntry extends React.Component {
 
   render() {
     const secondary = moment(this.props.chatMessage.created_at).fromNow();
-    if (this.props.chatMessage.profile_id_from === this.props.user.id) {
+
+    if (this.props.isFetching) {
+      return (
+        <div>
+          <div className="bump-tab-bar" />
+          <Row>
+            <Col xs={12} sm={6} smOffset={3}>
+              <Paper style={styles.paper} zDepth={1}>
+                <div style={styles.title}>
+                  <LoadingSpinner />
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else if (this.props.chatMessage.profile_id_from === this.props.user.id) {
       return (
         <ListItem
           primaryText={this.props.chatMessage.message}
@@ -35,6 +59,7 @@ class ChatListEntry extends React.Component {
 const mapStateToProps = state => ({
   chat: state.chat,
   user: state.user.profile,
+  isFetching: state.chat.isFetching,
 });
 
 export default connect(mapStateToProps)(ChatListEntry);
