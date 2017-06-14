39879ff710406964bc3e14bca9415fae643897e0
Tariq West <tariqwest@gmail.com>
2017-06-13 19:42:59 -0700

    Update radius input with value label

--- client/src/components/Signup/SearchRadiusInput.js
+++ client/src/components/Signup/SearchRadiusInput.js
@@ -1,30 +1,65 @@
 import React from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
 import Subheader from 'material-ui/Subheader';
-import Slider from 'material-ui/Slider';
+import Slider from 'material-ui-slider-label/Slider';
+import { cyan500, grey300 } from 'material-ui/styles/colors';
 
 const styles = {
   subheader: {
     textTransform: 'capitalize',
+    paddingBottom: '15px',
+  },
+  labelStyleOuter: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '50% 50% 50% 0',
+    background: cyan500,
+    position: 'absolute',
+    transform: 'rotate(-45deg)',
+    top: '-40px',
+    left: '-9px',
+  },
+  labelStyleOuterDisabled: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '50% 50% 50% 0',
+    background: grey300,
+    position: 'absolute',
+    transform: 'rotate(-45deg)',
+    top: '-40px',
+    left: '-9px',
+  },
+  labelStyleInner: {
+    transform: 'rotate(45deg)',
+    color: 'white',
+    textAlign: 'center',
+    position: 'relative',
+    top: '3px',
+    right: '0px',
+    fontSize: '10px',
   },
 };
 
 const SearchRadiusInput = ({ radius, onChange }) => (
-    <div>
-      <Subheader style={styles.subheader}>
-        {'Search radius'}
-      </Subheader>
-      <span id="radius_label" style={{ color: 'black', fontWeight: 'bold' }}>{radius}</span>
-      <Slider
-        defaultValue={5 / 100}
-        min={0}
-        max={1}
-        step={5 / 100}
-        value={radius / 100}
-        onChange={onChange}
-      />
-    </div>
+  <div>
+    <Subheader style={styles.subheader}>
+      {'Search radius'}
+    </Subheader>
+    <Slider
+      defaultValue={5 / 100}
+      min={0}
+      max={1}
+      step={5 / 100}
+      value={radius / 100}
+      onChange={onChange}
+      label={
+        <div style={radius > 0 ? styles.labelStyleOuter : styles.labelStyleOuterDisabled} >
+          <div style={styles.labelStyleInner}>
+            {radius}
+          </div>
+        </div>
+      }
+    />
+  </div>
 );
 
 export default SearchRadiusInput;
