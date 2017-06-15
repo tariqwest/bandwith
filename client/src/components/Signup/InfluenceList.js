a3b4459adfba27752eb36a475766adc0f4354d4f
April Ablon <ablonapril@gmail.com>
2017-06-14 19:50:10 -0700

    Update influence card rendering with flex box rows and columns, fixed widths, and better centered contents

--- client/src/components/Signup/InfluenceList.js
+++ client/src/components/Signup/InfluenceList.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { Card, CardHeader } from 'material-ui/Card';
+import { Row, Col } from 'react-flexbox-grid';
 
 const styles = {
-  chip: {
-    margin: 4,
+  card: {
+    margin: '0px 5px 5px 5px',
+    height: 78,
+    width: '100%',
   },
+  cardHeader: {
+    padding: '16px 0px 16px 16px',
+    height: 78,
+  },
+  cardHeaderTitle: {
+    paddingTop: 10,
+    paddingRight: 0,
+  },
+  column: { paddingRight: 2, paddingLeft: 2 },
+  row: { width: '100%' },
   wrapper: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -22,19 +35,51 @@ class InfluenceList extends Component {
   }
 
   render() {
-    return (
-      <div style={styles.wrapper}>
-        {this.props.selectedItems.length ?
-            this.props.selectedItems.map(item => (
-              <Card style={{ margin: '5px' }} key={item.name}>
-                <CardHeader style={{ padding: '16px 0px 16px 16px', display: 'inline-block' }} titleStyle={{ verticalAlign: 'middle' }} title={item.name} avatar={item.img} />
-                <div onClick={() => this.handleChipDelete(item.name)} className="remove-div">
-                  <i className="material-icons remove-button">clear</i>
-                </div>
-              </Card>),
-        ) : null}
-      </div>
-    );
+    if (this.props.selectedItems.length) {
+      const leftInfluences = this.props.selectedItems.filter((item, index) => index % 2 === 0);
+      const rightInfluences = this.props.selectedItems.filter((item, index) => index % 2 === 1);
+
+      return (
+        <div style={styles.wrapper}>
+          <Row style={styles.row}>
+            <Col xs={12} sm={6} style={styles.column}>
+              {leftInfluences.map(item => (
+                <Card style={styles.card} key={item.name} id="container-influence">
+                  <CardHeader
+                    id="base"
+                    style={styles.cardHeader}
+                    titleStyle={styles.cardHeaderTitle}
+                    title={item.name}
+                    avatar={item.img}
+                  />
+                  <div id="overlay-influence" onClick={() => this.handleChipDelete(item.name)}>
+                    <i className="material-icons">clear</i>
+                  </div>
+                </Card>
+                ),
+              )}
+            </Col>
+            <Col xs={12} sm={6} style={styles.column}>
+              {rightInfluences.map(item => (
+                <Card style={styles.card} key={item.name} id="container-influence">
+                  <CardHeader
+                    id="base"
+                    style={styles.cardHeader}
+                    titleStyle={styles.cardHeaderTitle}
+                    title={item.name}
+                    avatar={item.img}
+                  />
+                  <div id="overlay-influence" onClick={() => this.handleChipDelete(item.name)}>
+                    <i className="material-icons">clear</i>
+                  </div>
+                </Card>),
+              )}
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+    return (<div style={styles.wrapper} />);
   }
 }
 
