f593cac0e6fd752f244097e0b7bcd68bb7b1b6b4
calebcordry <caleb.cordry@gmail.com>
2017-06-15 17:25:26 -0700

    seed more connections

--- db/seeds/helpers/seedConnections.js
+++ db/seeds/helpers/seedConnections.js
@@ -2,36 +2,20 @@ const models = require('../../models');
 const { randomChoices } = require('./help');
 
 module.exports = (profiles) => {
-  const ids = profiles.map(profile => profile.id);
-  const indexesOne = randomChoices(ids, 10);
-  const indexesTwo = randomChoices(ids, 10);
+  let ids = profiles.map(profile => profile.id);
+  ids = ids.slice(0, ids.length);
+  console.log(ids);
+  const indexesOne = randomChoices(ids, 15);
+  console.log(indexesOne);
+  const indexesTwo = randomChoices(ids, 15);
+  console.log(indexesTwo);
  
   const connectionsInput = indexesOne.map((index, i) => ({
-    profile_id_1: ids[index],
-    profile_id_2: ids[indexesTwo[i]],
+    profile_id_1: index,
+    profile_id_2: indexesTwo[i],
     likes_1_2: true,
     likes_2_1: true,
-  }));
+  }))
 
-  // const connectionsInput = [
-  //   {
-  //     profile_id_1: ids[0],
-  //     profile_id_2: ids[1],
-  //     likes_1_2: true,
-  //     likes_2_1: true,
-  //   },
-  //   {
-  //     profile_id_1: ids[0],
-  //     profile_id_2: ids[2],
-  //     likes_1_2: true,
-  //     likes_2_1: true,
-  //   },
-  //   {
-  //     profile_id_1: ids[0],
-  //     profile_id_2: ids[3],
-  //     likes_1_2: true,
-  //     likes_2_1: true,
-  //   },
-  // ];
   return Promise.all(connectionsInput.map(input => models.Connection.forge(input).save()));
 };
